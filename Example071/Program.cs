9d93a68fcfff0199b2a98f9a2aca8ce7d50b07da
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-19 11:22:27 +0700

    Complete Example071

---
+++ Example071/Program.cs
@@ -0,0 +1,14 @@
+﻿// 71. Показать числа от N до 1, N задано
+int N = 10;
+PrintDigit(N);
+
+
+
+void PrintDigit(int N)
+{
+    if(N>0)
+    {
+        System.Console.WriteLine(N);
+        PrintDigit(N-1);
+    }
+}
