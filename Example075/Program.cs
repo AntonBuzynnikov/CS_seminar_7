4dbe9d3b4186fb9b20509db9905c64ed275e3618
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-19 11:23:53 +0700

    Complete Example075

---
+++ Example075/Program.cs
@@ -0,0 +1,21 @@
+﻿// 75.Показать фунцию Аккермана
+int n = 4;
+int m = 4;
+
+int AckermanFunction(int n, int m)
+{
+    if (n == 0) return m+1;
+    else
+        if(m == 0) return AckermanFunction(n-1,1);
+            else 
+                return AckermanFunction(n-1,AckermanFunction(n,m-1));
+}
+
+for(int i = 0; i < n;i++)
+{
+    for(int j = 0;j<m;j++)
+    {
+        System.Console.Write($"{AckermanFunction(j,i),4}");
+    }
+    System.Console.WriteLine();
+}
