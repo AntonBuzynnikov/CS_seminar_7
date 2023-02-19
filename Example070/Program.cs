d58fe9b033586b00639e55d02fe0fcb0666df8f9
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-19 11:22:08 +0700

    Complete Example070

---
+++ Example070/Program.cs
@@ -0,0 +1,13 @@
+﻿// 70. Показать натуральные числа от 1 до N, N задано
+int N = 10;
+int i = 1;
+PrintDigit(i,N);
+
+void PrintDigit(int i, int N)
+{
+    System.Console.WriteLine(i);
+    if(i<N) PrintDigit(i+1,N);
+}
+
+
+
